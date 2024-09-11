5489fef959bb73edba49d72ab82f0dbc6f30e597
Ke Jia <ke.jia@metropolia.fi>
2024-09-11 10:46:29 +0300

    Week4 Wed Morming PP Iteration 2 and Iteration 4 Part 1 by Ke Jia

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,12 +1,33 @@
-const Service = ({ icon, title, text }) => {
+import React, { useState } from 'react';
+
+const Service = ({item, onDelete}) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleText = () => {
+    setExpanded(!expanded);
+  };
+
+  const handleDelete = () => {
+    console.log(onDelete);
+    onDelete(item.id); // Pass the item's ID to the parent component for deletion
+  };
   return (
     <article className="service">
       <span className="service-icon">
-        <i className={icon}></i>
+        <i className={item.icon}></i>
       </span>
       <div className="service-info">
-        <h4 className="service-title">{title}</h4>
-        <p className="service-text">{text}</p>
+        <h4 className="service-title">{item.title}</h4>
+        {expanded && <p className="service-text">{item.text}</p>}
+      </div>
+      <div className="buttons">
+      <button onClick={toggleText}>
+        {expanded ? 'Hide Text' : 'Show Text'}
+      </button>
+      <br />
+      <button onClick={handleDelete} className="delete-button">
+        Delete
+      </button>
       </div>
     </article>
   );
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,15 +1,26 @@
+import { useState } from 'react';
 import { services } from '../data'
 import Title from './Title'
 import Service from './Service'
 
 const Services = () => {
+  const [servicesData, setServicesData] = useState(services);
+  const handleDeleteService = (serviceId) => {
+    // Filter out the item with the specified ID and update the state
+    const updatedServices = servicesData.filter((service) => service.id !== serviceId);
+    setServicesData(updatedServices);
+  };
   return (
     <section className='section services' id='services'>
       <Title title='our' subTitle='services' />
 
       <div className='section-center services-center'>
-        {services.map((service) => {
-          return <Service {...service} key={service.id} />
+        {servicesData.map((service) => {
+          return <Service
+                    key={service.id}
+                    item={service}
+                    onDelete={handleDeleteService}
+                 />
         })}
       </div>
     </section>
